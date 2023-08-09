24088560771e0aa663732cc2a356da2c8bb05244
jeongsangtae <wjdtkdxo55@naver.com>
2023-08-10 06:08:47 +0900

    React Complete Training Update Lesson Review - custom component rapper use(props.children)

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,11 +1,12 @@
 import React from "react";
 
 import ExpenseItem from "./ExpenseItem";
+import Card from "./Card";
 import "./Expenses.css";
 
 const Expenses = (props) => {
   return (
-    <div className="expenses">
+    <Card className="expenses">
       <ExpenseItem
         title={props.items[0].title}
         price={props.items[0].amount}
@@ -26,7 +27,7 @@ const Expenses = (props) => {
         price={props.items[3].amount}
         date={props.items[3].date}
       />
-    </div>
+    </Card>
   );
 };
 
