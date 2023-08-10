55a83db4c320a763b0c10a7fd45441e9ed441a7d
jeongsangtae <wjdtkdxo55@naver.com>
2023-08-10 19:18:15 +0900

    React Complete Training Update Lesson Review - componenet file make up

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import ExpnseDate from "./ExpenseDate";
-import Card from "./Card";
+import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
