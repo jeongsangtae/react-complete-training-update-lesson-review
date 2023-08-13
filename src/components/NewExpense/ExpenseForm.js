15e95f815542e39f5054754296ca6e9a09519841
jeongsangtae <wjdtkdxo55@naver.com>
2023-08-14 05:36:05 +0900

    React Complete Training Update Lesson Review - shared handler function add & README.md edit

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -14,40 +14,61 @@ const ExpenseForm = () => {
 
   const priceChangeHandler = (event) => {
     setEnteredPrice(event.target.value);
+    console.log(enteredPrice);
   };
 
   const dateChangeHandler = (event) => {
     setEnteredDate(event.target.value);
+    console.log(enteredDate);
   };
+
+  // 독립적인 핸들러 함수를 여러 개 두는 것 대신에 사용하는 대안 : 공유 핸들러 함수
+  // const inputChangeHandler = (identifier, value) => {
+  //   if (identifier === "title") {
+  //     setEnteredTitle(value);
+  //   } else if (identifier === "price") {
+  //     setEnteredPrice(value);
+  //   } else {
+  //     setEnteredDate(value);
+  //   }
+  // };
 
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input
+          <input type="text" onChange={titleChangeHandler} />
+          {/* <input
             type="text"
-            onChange={titleChangeHandler}
-            value={enteredTitle}
-          />
+            onChange={(event) => {
+              inputChangeHandler("title", event.target.value);
+            }}
+          /> */}
         </div>
         <div className="new-expense__control">
           <label>Price</label>
-          <input
+          <input type="number" onChange={priceChangeHandler} />
+          {/* <input
             type="number"
             min="10"
             step="10"
-            onChange={priceChangeHandler}
-          />
+            onChange={(event) => {
+              inputChangeHandler("price", event.target.value);
+            }}
+          /> */}
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input
+          <input type="date" onChange={dateChangeHandler} />
+          {/* <input
             type="date"
             min="2021-01-01"
             max="2023-12-31"
-            onChange={dateChangeHandler}
-          />
+            onChange={(event) => {
+              inputChangeHandler("date", event.target.value);
+            }}
+          /> */}
         </div>
       </div>
       <div className="new-expense__actions">
