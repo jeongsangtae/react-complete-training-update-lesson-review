2b6cee08a36824a4986167d5abbc2b418a29e5da
jeongsangtae <wjdtkdxo55@naver.com>
2023-08-15 04:50:13 +0900

    React Complete Training Update Lesson Review - derived/computed state

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -8,6 +8,17 @@ import "./Expenses.css";
 const Expenses = (props) => {
   const [filterYear, setFilterYear] = useState("2023");
 
+  // 파생/계산된 상태 사용
+  // let filterInfoText = "2020, 2021 & 2022";
+
+  // if (filterYear === "2020") {
+  //   filterInfoText = "2021, 2022 & 2023";
+  // } else if (filterYear === "2021") {
+  //   filterInfoText = "2020, 2022 & 2023";
+  // } else if (filterYear === "2022") {
+  //   filterInfoText = "2020, 2021 & 2023";
+  // }
+
   const filterExpenseDate = (filterData) => {
     setFilterYear(filterData);
     console.log(filterData);
@@ -19,6 +30,8 @@ const Expenses = (props) => {
         onFilterExpenseData={filterExpenseDate}
         selectYear={filterYear}
       />
+      {/* 파생/계산된 상태의 결과 값를 보여주는 p태그 */}
+      {/* <p>Data for years {filterInfoText} is hidden.</p> */}
       <ExpenseItem
         title={props.items[0].title}
         price={props.items[0].price}
